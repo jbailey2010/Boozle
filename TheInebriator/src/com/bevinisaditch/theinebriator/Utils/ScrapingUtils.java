68223177fa477277f8bb3ba324b3246b66d779ea
Jeff Bailey <bailey27@illinois.edu>
2014-02-16 13:48:53 -0600

    JSoup stuff should work better

--- TheInebriator/src/com/bevinisaditch/theinebriator/Utils/ScrapingUtils.java
+++ TheInebriator/src/com/bevinisaditch/theinebriator/Utils/ScrapingUtils.java
@@ -1,6 +1,8 @@
 package com.bevinisaditch.theinebriator.Utils;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -16,23 +18,23 @@ public class ScrapingUtils {
 	 * @return the text that was parsed
 	 * @throws IOException
 	 */
-	public static String handleQuery(String url, String params) throws IOException
+	public static List<String> handleQuery(String url, String params) throws IOException
 	{
 		StringBuilder result = new StringBuilder(5000);
-		Document doc = Jsoup.connect(url).userAgent(ua).get();
+		Document doc = Jsoup.connect(url).userAgent(ua).timeout(0).get();
         Elements links = doc.select(params);
 
         for (Element element : links) 
         {
         	result.append(element.text() + "\n");
         }
-        return result.toString();
+        return Arrays.asList(result.toString().split("\n"));
 	}
 	
 	/**
 	 * Just in case the web site doesn't like using the chrome user agent/timeout being infinite is bad, this will work generically
 	 */
-	public static String handleQueryNoUA(String url, String params) throws IOException
+	public static List<String> handleQueryNoUA(String url, String params) throws IOException
 	{
 		StringBuilder result = new StringBuilder(5000);
 		Document doc = Jsoup.connect(url).get();
@@ -42,14 +44,14 @@ public class ScrapingUtils {
         {
         	result.append(element.text() + "\n");
         }
-        return result.toString();
+        return Arrays.asList(result.toString().split("\n"));
 	}
 	
 	/**
 	 * This is the query function for a list
 	 * called twice+ with the same page
 	 */
-	public static String handleQueryMulti(Document doc, String url, String params) throws IOException
+	public static List<String> handleQueryMulti(Document doc, String url, String params) throws IOException
 	{
 		StringBuilder result = new StringBuilder(5000);
         Elements links = doc.select(params);
@@ -57,6 +59,6 @@ public class ScrapingUtils {
         {
         	result.append(element.text() + "\n");
         }
-        return result.toString();
+        return Arrays.asList(result.toString().split("\n"));
 	}
 }
