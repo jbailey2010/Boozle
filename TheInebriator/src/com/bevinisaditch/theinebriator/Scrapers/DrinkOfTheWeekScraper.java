d7a62465fda77ae818d4ba3095de7f77a5af3ced
michael.b.willingham <michael.b.willingham@gmail.com>
2014-02-20 18:04:01 -0600

    Gets the URLS for all the drink pages. Need to connect to the individual pages to get the individual drink information now.

--- TheInebriator/src/com/bevinisaditch/theinebriator/Scrapers/DrinkOfTheWeekScraper.java
+++ TheInebriator/src/com/bevinisaditch/theinebriator/Scrapers/DrinkOfTheWeekScraper.java
@@ -19,16 +19,30 @@ public class DrinkOfTheWeekScraper {
 	
 	public static void connectToWebsite() {
 		try {
+			
+			//Connect to the website
 			doc = Jsoup.connect("http://www.drinkoftheweek.com/list-drinks-a-z/")
 					.data("query", "Java")
 					.userAgent("Mozilla")
 					.cookie("auth", "token")
 					.timeout(3000)
 					.post();
+			
+			//Get drinks in the 'drinkgroup' html elements
 			Elements all_drinks = doc.getElementsByClass("drinkgroup");
 			
+			//For each drink group (a, b, c, d, ... z)
 			for (Element drink_group: all_drinks) {
-				System.out.println(drink_group.getElementsByAttribute("href").text());
+				
+				//Get the href tags
+				Elements urls = drink_group.getElementsByAttribute("href");
+				
+				//Get URL from href tag
+				for (Element urlTag : urls) {
+					String url = urlTag.attr("href");
+					System.out.println(url);
+				}
+				
 				
 			}
 		} catch (IOException e) {
