be58ce17fa243646bf71764c4c8581156ffd357e
annies4in <binjrajkaakash@gmail.com>
2014-02-23 20:57:00 -0600

    Being able to run the test case for GoodCocktails
    Had to change some configuration items

--- TheInebriator/src/com/bevinisaditch/theinebriator/Scrapers/GoodCocktailsScraper.java
+++ TheInebriator/src/com/bevinisaditch/theinebriator/Scrapers/GoodCocktailsScraper.java
@@ -8,14 +8,19 @@ import com.bevinisaditch.theinebriator.Utils.ScrapingUtils;
 
 public class GoodCocktailsScraper {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		scrapeDrinks();
 
 	}
 
 	public static void scrapeDrinks() throws IOException {
 		List<String> links = new ArrayList<String>();
+		
+		/*need to decide what param to give here*/
 		links = ScrapingUtils.handleQuery("http://www.goodcocktails.com/recipes/browse_drinks.php?letter=ALL", "");
+		
+		
 		
 	}
 	
