a5f5e606421c63d83161fcb6928ff2830ef2908c
annies4in <binjrajkaakash@gmail.com>
2014-02-23 20:33:45 -0600

    Created skeletons for goodcocktails scraper and test

---
+++ TheInebriator/test/com/bevinisaditch/theinebriator/Scraper/GoodCocktailsScraperTest.java
@@ -0,0 +1,19 @@
+package com.bevinisaditch.theinebriator.Scraper;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import com.bevinisaditch.theinebriator.Scrapers.GoodCocktailsScraper;
+
+public class GoodCocktailsScraperTest {
+
+	@Test
+	public void test() throws IOException {
+		GoodCocktailsScraper.scrapeDrinks();
+		fail("Not yet implemented");
+	}
+
+}
