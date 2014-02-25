4363d142ffdad7e0c6337ff6e683798b82237339
annies4in <binjrajkaakash@gmail.com>
2014-02-24 18:07:44 -0600

    finished the method for main scraper for goodCocktails

--- TheInebriator/src/com/bevinisaditch/theinebriator/Scrapers/GoodCocktailsScraper.java
+++ TheInebriator/src/com/bevinisaditch/theinebriator/Scrapers/GoodCocktailsScraper.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import com.bevinisaditch.theinebriator.Utils.ScrapingUtils;
 
 public class GoodCocktailsScraper {
@@ -15,14 +19,32 @@ public class GoodCocktailsScraper {
 	}
 
 	public static void scrapeDrinks() throws IOException {
-		List<String> drinkLinks = new ArrayList<String>();
+		
+		//making connection to the website
+		Document doc = ScrapingUtils.makeConnection("http://www.goodcocktails.com/recipes/browse_drinks.php?letter=ALL");
 		
 		/*need to decide what param to give here*/
-		drinkLinks = ScrapingUtils.handleQuery("http://www.goodcocktails.com/recipes/browse_drinks.php?letter=ALL", "div#browse-drinks");
+		//drinkLinks = ScrapingUtils.handleQuery("http://www.goodcocktails.com/recipes/browse_drinks.php?letter=ALL", "div#browse-drinks");
 		
 		//ScrapingUtils.printQueryResultSize("http://www.goodcocktails.com/recipes/browse_drinks.php?letter=ALL", "a[href^=mixed_drink.php?drinkID=]", false, null);
 		
+		//Finding all the drinks
+		Elements elements = doc.select("a[href^=mixed_drink.php?drinkID=]");
+		List<String> drinkLinks = new ArrayList<String>();
 		
+		//Extracting the links for each drink
+		for(Element element : elements) {
+			drinkLinks.add(element.attr("href").toString());
+		}
+		
+		for(int i = 0; i < drinkLinks.size(); i++) {
+			String url = "http://www.goodcocktails.com/recipes/" + drinkLinks.get(i);
+			scrapeIndividualDrink(url);
+		}
+		
+	}
+	
+	public static void scrapeIndividualDrunk(String url) {
 		
 	}
 	
