a3f57b858cf1cd021dcfe8e1d0dde842c2af999d
Kirara35 <jboctorloz@gmail.com>
2014-03-19 18:35:11 -0500

    empty test file

---
+++ TheInebriatorTestTest/src/com/bevinisaditch/theinebriator/Database/test/DataBaseReaderTest.java
@@ -0,0 +1,14 @@
+package com.bevinisaditch.theinebriator.Database.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DataBaseReaderTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
