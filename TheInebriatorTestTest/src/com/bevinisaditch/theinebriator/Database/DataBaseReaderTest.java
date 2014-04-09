e02f58cd79038e315f82ab300fe054fbe692681d
TubTubbaroo <Jasontermaat@gmail.com>
2014-04-09 03:50:11 -0500

    Test for db

--- TheInebriatorTestTest/src/com/bevinisaditch/theinebriator/Database/DataBaseReaderTest.java
+++ TheInebriatorTestTest/src/com/bevinisaditch/theinebriator/Database/DataBaseReaderTest.java
@@ -19,39 +19,38 @@ public class DataBaseReaderTest extends AndroidTestCase {
 
 	public void testReadDatabase()
 	{
-		DrinkDatabaseHandler handler = new DrinkDatabaseHandler(this.getContext());
-		allDrinks = handler.getAllDrinks();
+		DrinkDatabaseHandler dbh = new DrinkDatabaseHandler(this.getContext());
+		allDrinks = dbh.getAllDrinks();
 		ArrayList<Ingredient> ingredients = new ArrayList<Ingredient>();
 		ingredients.add(new Ingredient("Brandy or Rum (Light)", "1 1/2", "oz."));
 		ingredients.add(new Ingredient("Cream / Half & Half", "1/2", "oz."));
 		ingredients.add(new Ingredient("Honey", "1", "Tbsp."));
-		Drink sampleDrink = new Drink("Zoom", Drink.Rating.THUMBSNULL, ingredients, "Pour all ingredients into a shaker with ice. Shake and strain into a chilled martini glass.", 0);
+		Drink sampleDrink = new Drink("Zoom", Drink.Rating.THUMBSNULL, ingredients, "Pour all ingredients into a shaker with ice. Shake and strain into a chilled martini glass.", 13380);
 		assertDrinkFound(sampleDrink);
 		ingredients = new ArrayList<Ingredient>();
 		ingredients.add(new Ingredient("Vodka", "1", "oz."));
 		ingredients.add(new Ingredient("Raspberry Liqueur", "1", "oz."));
 		ingredients.add(new Ingredient("Lemon-Lime Soda or Club Soda / Carbonated Water", "", ""));
-		sampleDrink = new Drink("Zipperhead", Drink.Rating.THUMBSNULL, ingredients, "Pour vodka and raspberry liqueur into a glass with ice. Fill it with lemon-lime soda or soda water. Do not stir or shake.", 0);
+		sampleDrink = new Drink("Zipperhead", Drink.Rating.THUMBSNULL, ingredients, "Pour vodka and raspberry liqueur into a glass with ice. Fill it with lemon-lime soda or soda water. Do not stir or shake.", 13376);
 		assertDrinkFound(sampleDrink);
 		ingredients = new ArrayList<Ingredient>();
 		ingredients.add(new Ingredient("Rum (Light)", "2", "oz."));
 		ingredients.add(new Ingredient("Strawberry Liqueur (Optional)", "1/2", "oz."));
 		ingredients.add(new Ingredient("Sweet & Sour Mix", "1", "oz."));
 		ingredients.add(new Ingredient("", "3", "Strawberries"));
-		sampleDrink = new Drink("Strawberry Daiquiri", Drink.Rating.THUMBSNULL, ingredients, "Put all ingredients into a blender with ice. Blend until smooth and pour into a glass. If it is too thick add more sweet and sour.", 0);
+		sampleDrink = new Drink("Strawberry Daiquiri", Drink.Rating.THUMBSNULL, ingredients, "Put all ingredients into a blender with ice. Blend until smooth and pour into a glass. If it is too thick add more sweet and sour.", 13029);
 		assertDrinkFound(sampleDrink);
 	}
 	
 	private void assertDrinkFound(Drink sampleDrink)
 	{
-		boolean foundDrink = false;
-		for (Drink currDrink : allDrinks)
+		Drink otherDrink = allDrinks.get(sampleDrink.getId());
+		boolean found = sampleDrink.equals(otherDrink);
+		if (!found)
 		{
-			if (currDrink.equals(sampleDrink))
-			{
-				foundDrink = true;
-			}
+			System.out.println("Looked for: " + sampleDrink.getId() + ", " + sampleDrink.getName());
+			System.out.println("Found:      " + otherDrink.getId() + ", " + otherDrink.getName());
 		}
-		assertTrue(foundDrink);
+		assertTrue(found);
 	}
 }
