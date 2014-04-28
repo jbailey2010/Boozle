d2457c12dc12a70a499fe2f4d19f71f0776aaefc
TubTubbaroo <Jasontermaat@gmail.com>
2014-04-28 17:19:06 -0500

    Updated tests to new database

--- TheInebriatorTestTest/src/com/bevinisaditch/theinebriator/Database/DataBaseReaderTest.java
+++ TheInebriatorTestTest/src/com/bevinisaditch/theinebriator/Database/DataBaseReaderTest.java
@@ -21,49 +21,10 @@ public class DataBaseReaderTest extends AndroidTestCase {
 		ArrayList<Ingredient> ingredients;
 		Drink sampleDrink;
 		ingredients = new ArrayList<Ingredient>();
-		ingredients.add(new Ingredient("1 part DeKuyper Pucker sour-apple schnapps", "", ""));
-		ingredients.add(new Ingredient("1 part Absolut vodka", "", ""));
-		sampleDrink = new Drink("2-Point Play", Drink.Rating.THUMBSNULL, ingredients, "Pour ingredients into a cocktail shaker with ice. Shake and strain into a chilled martini glass. Garnish with an apple wedge.", 0);
+		sampleDrink = new Drink("American Beauty Shot", Drink.Rating.THUMBSNULL, ingredients, "Mix everything in a cocktail shaker with ice. Strain into a chilled cocktail glass or a highball glass filled with ice.", 0);
 		assertDrinkFound(sampleDrink);
 		ingredients = new ArrayList<Ingredient>();
-		ingredients.add(new Ingredient("3 oz Cannabis Vodka (or make your own)", "", ""));
-		ingredients.add(new Ingredient("Dry vermouth (enough to coat the glass)", "", ""));
-		ingredients.add(new Ingredient("One hit super premium sativa or indica", "", ""));
-		sampleDrink = new Drink("420 Martini", Drink.Rating.THUMBSNULL, ingredients, "Coat a chilled cocktail glass with the vermouth. Chill vodka in a shaker with ice. Shake well and strain into the cocktail glass. Take the bong hit, hold, and take a sip of the martini. Exhale.", 1);
-		assertDrinkFound(sampleDrink);
-		ingredients = new ArrayList<Ingredient>();
-		ingredients.add(new Ingredient("1 tsp brown sugar", "", ""));
-		ingredients.add(new Ingredient("1 part lemon juice", "", ""));
-		ingredients.add(new Ingredient("1 part Bacardi Oro", "", ""));
-		ingredients.add(new Ingredient("1 part Bacardi 151", "", ""));
-		ingredients.add(new Ingredient("1 part 42 BELOW Passion Fruit", "", ""));
-		ingredients.add(new Ingredient("1 part unsweetened pineapple juice", "", ""));
-		ingredients.add(new Ingredient("1 part fresh lime juice", "", ""));
-		ingredients.add(new Ingredient("1 part passion fruit syrup", "", ""));
-		ingredients.add(new Ingredient("1 dash angostura bitters", "", ""));
-		sampleDrink = new Drink("42Zombie", Drink.Rating.THUMBSNULL, ingredients, "Dissolve sugar and lemon juice, then add all other ingredients and shake well with crushed ice. Pour entire contents into a collins glass and garnish with lime wedge.", 2);
-		assertDrinkFound(sampleDrink);
-		ingredients = new ArrayList<Ingredient>();
-		sampleDrink = new Drink("4th of July Watermelon Cocktail", Drink.Rating.THUMBSNULL, ingredients, "", 3);
-		assertDrinkFound(sampleDrink);
-		ingredients = new ArrayList<Ingredient>();
-		ingredients.add(new Ingredient("1 1/2 oz American Gin", "", ""));
-		ingredients.add(new Ingredient("3/4 oz Dry vermouth", "", ""));
-		ingredients.add(new Ingredient("3/4 oz Blanc vermouth", "", ""));
-		ingredients.add(new Ingredient("2 Dashes orange bitters", "", ""));
-		sampleDrink = new Drink("50/50 Split", Drink.Rating.THUMBSNULL, ingredients, "In a mixing glass, add spirits and mixers. Fill with ice and stir. Strain into a chilled martini glass and garnish with a lemon twist.", 4);
-		assertDrinkFound(sampleDrink);
-		ingredients = new ArrayList<Ingredient>();
-		sampleDrink = new Drink("A Happy Jewish New Year", Drink.Rating.THUMBSNULL, ingredients, "", 6);
-		assertDrinkFound(sampleDrink);
-		ingredients = new ArrayList<Ingredient>();
-		sampleDrink = new Drink("Aberdeen Flip", Drink.Rating.THUMBSNULL, ingredients, "", 7);
-		assertDrinkFound(sampleDrink);
-		ingredients = new ArrayList<Ingredient>();
-		ingredients.add(new Ingredient("1 oz anisette", "", ""));
-		ingredients.add(new Ingredient("1 oz Herbsaint anise liqueur", "", ""));
-		ingredients.add(new Ingredient("Soda water", "", ""));
-		sampleDrink = new Drink("Absinthe Frapp&eacute;", Drink.Rating.THUMBSNULL, ingredients, "Pour the anisette and Herbsaint into an old-fashioned glass filled with ice, stir and add soda water.", 8);
+		sampleDrink = new Drink("Algonquin Cocktail", Drink.Rating.THUMBSNULL, ingredients, "Combine all ingredients in cocktail shaker. Stir with ice and strain into a cocktail glass or over ice in a rocks glass.", 1);
 		assertDrinkFound(sampleDrink);
 	}
 	
@@ -71,7 +32,7 @@ public class DataBaseReaderTest extends AndroidTestCase {
 	{
 		boolean foundDrink = false;
 		Drink otherDrink = allDrinks.get(sampleDrink.getId());
-		foundDrink = sampleDrink.equals(otherDrink);
+		foundDrink = sampleDrink.getName().equals(otherDrink.getName()) && sampleDrink.getInstructions().equals(otherDrink.getInstructions());
 		if (!foundDrink)
 		{
 			//System.out.println("Looked for ID: " + sampleDrink.getId() + "\tName: " + sampleDrink.getName());
